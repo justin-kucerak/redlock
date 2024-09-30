d215f5bf06caa6c853c2c98074fc745bd199dba4
justin-kucerak <justin@justinkucerak.com>
2024-09-30 01:11:16 -0400

    export class and RedisClient interface

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { Redlock } from './Redlock';
+export { RedisClient } from './RedisClient';
+export { IORedisAdapter } from './adapters/IORedisAdapter';
+export { NodeRedisAdapter } from './adapters/NodeRedisAdapter';
